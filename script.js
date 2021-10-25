ce56b7c53d618102cc322384a43ed8d57636d1a5
Larissa Adler Ewerton Coelho <91379960+LaraAEC@users.noreply.github.com>
2021-10-25 12:35:19 -0300

    Adicionado Jogo de Trunfo Naruto Shippuden
    link do Jogo Super Trunfo Naruto Shippuden

---
+++ script.js
@@ -0,0 +1,402 @@
+var carta1 = {
+  nome: "Tsunade",
+  imagem:
+    "https://i.pinimg.com/736x/e1/9c/89/e19c89b3fa618f3313ad7859480f72fa.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 7,
+    jutsu: 8
+  }
+};
+
+var carta2 = {
+  nome: "Ino Yamanaka",
+  imagem:
+    "https://i.pinimg.com/236x/a9/7d/74/a97d7445e9f6650834742049148f9e8b.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 6,
+    justu: 5
+  }
+};
+
+var carta3 = {
+  nome: "Minato Namikaze",
+  imagem:
+    "https://i.pinimg.com/564x/33/2f/02/332f02324f3b413d4c2e03dd6821f09b.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    jutsu: 10
+  }
+};
+
+var carta4 = {
+  nome: "Hinata Hyuga",
+  imagem:
+    "https://i.pinimg.com/280x280_RS/76/ed/7b/76ed7b1bdab66ae4bb29a72c51e84808.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    jutsu: 6
+  }
+};
+
+var carta5 = {
+  nome: "Sakura Haruno",
+  imagem: "https://sm.ign.com/ign_br/screenshot/default/2_seev.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 10,
+    jutsu: 10
+  }
+};
+
+var carta6 = {
+  nome: "Sasuke Uchiha",
+  imagem:
+    "https://i.pinimg.com/736x/c5/ec/a2/c5eca2d834452f9a91d98cf2b13e76bb.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    jutsu: 10
+  }
+};
+
+var carta7 = {
+  nome: "Kakashi Hatake",
+  imagem:
+    "https://i.pinimg.com/originals/00/88/3f/00883fe2c026d4e31b8ddcc1e11da2e0.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 9,
+    jutsu: 8
+  }
+};
+
+var carta8 = {
+  nome: "Shikamaru Nara",
+  imagem:
+    "https://pbs.twimg.com/profile_images/1377304372720336900/Fd1mwLIa_400x400.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    jutsu: 6
+  }
+};
+
+var carta9 = {
+  nome: "Itachi Uchiha",
+  imagem:
+    "https://i.pinimg.com/originals/8e/2a/e4/8e2ae47845d6963f3aaf6abe2efbc8ad.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 9,
+    jutsu: 10
+  }
+};
+
+var carta10 = {
+  nome: "Temari",
+  imagem:
+    "https://i.pinimg.com/736x/82/98/55/829855c065ab625bcac4abd0f1643c69.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    jutsu: 6
+  }
+};
+
+var carta11 = {
+  nome: "Gaara",
+  imagem:
+    "https://i.pinimg.com/564x/5f/9c/2c/5f9c2c0ab037f866aa6eb1c2e96ed7d4.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 8,
+    jutsu: 7
+  }
+};
+
+var carta12 = {
+  nome: "Neji Hyuga",
+  imagem:
+    "https://narutokonoha.com/wp-content/uploads/2021/05/Quem-e-o-que-matou-Neji-Hyuuga.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 6,
+    jutsu: 7
+  }
+};
+
+var carta13 = {
+  nome: "Naruto Uzumaki",
+  imagem:
+    "https://i.pinimg.com/736x/6c/e3/87/6ce3876e15d1a50f35ba5d02414de205--naruto-the-movie-naruto-the-last.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    jutsu: 9
+  }
+};
+
+var carta14 = {
+  nome: "Hiruzen Sarutobi sensei",
+  imagem:
+    "https://i.pinimg.com/originals/e2/e3/21/e2e3217d529a25957e5ae8986871cdb6.png",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    jutsu: 9
+  }
+};
+
+var carta15 = {
+  nome: "Jiraya",
+  imagem:
+    "https://i.pinimg.com/474x/51/ac/7d/51ac7d5c26281890d77efc7d686c7d2e.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    jutsu: 8
+  }
+};
+
+var carta16 = {
+  nome: "Obito Uchiha",
+  imagem:
+    "https://i.pinimg.com/236x/58/ab/cd/58abcdd453e611b646b8b5a8e0cc36d3.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 8,
+    jutsu: 10
+  }
+};
+
+var carta17 = {
+  nome: "Kushina Uzumaki",
+  imagem:
+    "http://pm1.narvii.com/6403/a6056896756bbd8e3301c11ddbb8c4537524ef4f_00.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    jutsu: 8
+  }
+};
+
+var carta18 = {
+  nome: "Assuma Sarutobi sensei",
+  imagem:
+    "https://i.pinimg.com/736x/67/44/d6/6744d692f2b742f9bd5132297ec471ff.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    jutsu: 6
+  }
+};
+
+var carta19 = {
+  nome: "Kurenai sensei",
+  imagem:
+    "https://i.pinimg.com/736x/9b/86/81/9b8681961a6c8b95249a3c1043548fa6.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 8,
+    jutsu: 6
+  }
+};
+
+var carta20 = {
+  nome: "Iruka sensei",
+  imagem:
+    "http://pm1.narvii.com/6563/2bf1521e5c67c266745ce32cd1b8f2009a696c61_00.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    jutsu: 4
+  }
+};
+
+var carta21 = {
+  nome: "Konan",
+  imagem:
+    "https://i.pinimg.com/originals/43/42/19/434219d2ea65c3aad0494a69b96896ba.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 10,
+    jutsu: 10
+  }
+};
+
+var carta22 = {
+  nome: "Orochimaru",
+  imagem:
+    "https://i.pinimg.com/736x/53/0d/07/530d07ce8589284dbe84193b24a04e44.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    jutsu: 8
+  }
+};
+
+var carta23 = {
+  nome: "Kiba",
+  imagem:
+    "https://i.pinimg.com/550x/98/4b/59/984b5919c508abded5bd0862789dea57.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 6,
+    jutsu: 4
+  }
+};
+
+var carta24 = {
+  nome: "Konohamaru Sarutobi",
+  imagem:
+    "http://2.bp.blogspot.com/-k12Fz85lVnc/TtZXBT2HWdI/AAAAAAAADlg/Oz3dNEbPUdg/s1600/Konohamaru2.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 4,
+    jutsu: 3
+  }
+};
+
+var carta25 = {
+  nome: "Killer Bee",
+  imagem:
+    "http://pm1.narvii.com/6333/1e82766042537e161a7d1f365f00b165db51c72f_00.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    jutsu: 9
+  }
+};
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25
+];
+
+var cartaMaquina = 0;
+var cartaJogador = 0;
+
+function sortearCarta() {
+  var numCartaMaquina = parseInt(Math.random() * 25);
+  cartaMaquina = cartas[numCartaMaquina];
+
+  var numCartaJogador = parseInt(Math.random() * 25);
+  while (numCartaMaquina == numCartaJogador) {
+    numCartaJogador = parseInt(Math.random() * 25);
+  }
+  cartaJogador = cartas[numCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  console.log("chamou");
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Você Venceu</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Você Perdeu</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+
+  var tagHTML = "<div id= 'opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
